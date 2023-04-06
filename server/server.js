d645df43f9511533364e78f67ffdedba6d983f2a
Sung Chi <chis5@gene.com>
2023-04-06 13:57:45 -0700

    switched to 3.5 turbo

--- server/server.js
+++ server/server.js
@@ -19,10 +19,10 @@ app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: "gpt-3.5-turbo",
       prompt: `${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
- max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      max_tokens: 4096, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
@@ -35,4 +35,4 @@ app.post('/', async (req, res) => {
     res.status(500).send(error || 'Something went wrong');
   }
 });
-app.listen(5001, () => console.log('AI server started on http://localhost:8001'));
+app.listen(5001, () => console.log('AI server started on http://localhost:5001'));
